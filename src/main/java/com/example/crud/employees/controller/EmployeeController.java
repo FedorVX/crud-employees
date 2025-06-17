12c969b01d6a4ef81cf66be9a362fd487abd1811
Fedor <valayev2005@gmail.com>
2025-06-17 22:57:52 +0700

    Complete CRUD app

--- src/main/java/com/example/crud/employees/controller/EmployeeController.java
+++ src/main/java/com/example/crud/employees/controller/EmployeeController.java
@@ -3,9 +3,12 @@ package com.example.crud.employees.controller;
 import com.example.crud.employees.model.Employee;
 import com.example.crud.employees.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class EmployeeController {
@@ -15,8 +18,7 @@ public class EmployeeController {
     private EmployeeService employeeService;
     @GetMapping("/")
     public String viewHomePage(Model model){
-        model.addAttribute("listEmployees", employeeService.getAllEmployees());
-        return "index";
+        return findPaginated(1, "firstName", "asc", model); //default start app
     }
 
 
@@ -47,6 +49,31 @@ public class EmployeeController {
     public String deleteEmployee(@RequestParam("id") long id){
         employeeService.deleteById(id);
         return "redirect:/";
+    }
+
+
+    // /page/1?sortField=name&sortDir=asc
+
+
+    @GetMapping("/page/{pageNo}")
+    public String findPaginated(@PathVariable(value = "pageNo") int pageNo,
+                                @RequestParam(value= "sortField", defaultValue = "firstName", required=false) String sortField,
+                                @RequestParam(value= "sortDirection", defaultValue = "asc", required=false) String sortDirection,
+                                Model model){
+        int pageSize = 5;
+
+        Page<Employee> page = employeeService.findPaginated(pageNo, pageSize, sortField, sortDirection);
+        List<Employee> listEmployees = page.getContent();
+
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("listEmployees", listEmployees);
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDirection", sortDirection);
+        model.addAttribute("reverseSortDirection", sortDirection.equals("asc") ? "desc" : "asc");
+        return "index";
     }
 
 }
